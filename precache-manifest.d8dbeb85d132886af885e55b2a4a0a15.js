c12003c257a8013b3ebe8c792b8c89f9a289a2b4
Andrew Jenkins <ajenkins970@gmail.com>
2019-07-04 00:16:41 +0100

    Updates

---
+++ precache-manifest.d8dbeb85d132886af885e55b2a4a0a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2268b40f2cc7c0755a72a33a3c252a",
+    "url": "/andrew-jenkins-web-app/index.html"
+  },
+  {
+    "revision": "a0b87520249a280d3d85",
+    "url": "/andrew-jenkins-web-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/andrew-jenkins-web-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a0b87520249a280d3d85",
+    "url": "/andrew-jenkins-web-app/static/js/main.dae6b242.chunk.js"
+  },
+  {
+    "revision": "37588d90889f1779c16c",
+    "url": "/andrew-jenkins-web-app/static/js/runtime~main.f681b78d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/andrew-jenkins-web-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
