498dc52723880d534387f55367558596555a3a4d
Andrew Jenkins <ajenkins970@gmail.com>
2019-07-05 19:18:10 +0100

    Add image assets.

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 
 import PortfolioCard from './portfolio-card';
 
-import ionaRacing from '../assets/image/iona-racing.jpg';
+import portfolioItems from '../assets/json/portfolio-items.json';
 
 class Portfolio extends Component {
+
+  renderCards() {
+    if (!(portfolioItems instanceof Array)) {
+      throw new Error('Portfolio items cannot be rendered.');
+    }
+
+    return portfolioItems.map(item =>
+      <PortfolioCard
+        key={item.name}
+        date={item.date}
+        description={item.description}
+        name={item.name}
+        image={require(`../assets/image/${item.image}`)}
+        type={item.type}
+      />
+    );
+  }
 
   render() {
     return (
       <div className='portfolio segment'>
-        <PortfolioCard date='06/06/2017' description='12345' name='Iona Racing' image={ionaRacing} type='Logo' />
+        {this.renderCards()}
       </div>
     );
   }
